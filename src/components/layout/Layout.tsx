2f12c9bdbefadfca1ad1aaee3a1f781b9df05674
john mwangi <modernacyber@gmail.com>
2024-05-14 22:02:16 +0300

    dark toggle

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 }
 const Layout:React.FC<LayoutProps> = ({children}) => {
   return (
-    <div className=''>
+    <div className='dark:bg-black '>
         <Navbar/>
         {children}
         <Footer/>
